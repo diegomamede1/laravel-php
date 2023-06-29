1e0b740d30cd3df13d6cf33fd1296b25282221b6
Diego Mamede <diegomamede244@gmail.com>
2023-06-29 08:19:16 -0300

    adicionando tabela e botões editar e excluir

--- myapp/resources/views/pessoas/index.blade.php
+++ myapp/resources/views/pessoas/index.blade.php
@@ -1,16 +1,69 @@
 @extends('base')
 
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid;
+    }
+
+    table {
+        width: 100%;
+    }
+
+    /* table {
+        width: 80%;
+        border-collapse: collapse;
+    }
+
+    td,
+    th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    } */
+</style>
 
 @section('content')
     @foreach ($Pessoas as $Pessoa)
         <a href="{{ route('pessoas.show', $Pessoa) }}">
+            <style>
+                table,
+                th,
+                td {
+                    border: 1px solid;
+                }
+
+                table {
+                    width: 50%;
+                }
+            </style>
+
             <table>
-                {{ $Pessoa->name }} <br>
-                {{ $Pessoa->sobrenome }} <br>
-                {{ $Pessoa->endereco }} <br>
-                {{ $Pessoa->telefone }} <br>
-                {{ $Pessoa->cpf }} <br>
+                <tr>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>Endereco</th>
+                    <th>Telefone</th>
+                    <th>Cpf</th>
+                    <th>Ações</th>
+                </tr>
+                <tr>
+                    <td>{{ $Pessoa->nome }}<br></td>
+                    <td>{{ $Pessoa->sobrenome }}<br></td>
+                    <td>{{ $Pessoa->endereco }}<br></td>
+                    <td>{{ $Pessoa->telefone }}<br></td>
+                    <td>{{ $Pessoa->cpf }}<br></td>
+                    <td><button>Editar</button><br>
+                        <button>Excluir</button></td>
+
+                </tr><br>
             </table>
+
         </a>
         <hr>
     @endforeach
