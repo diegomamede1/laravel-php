44d8010b8a24eda6ed7e6301d2fddc870961728d
Diego Mamede <diegomamede244@gmail.com>
2023-06-29 09:34:39 -0300

    ajustes no layout

--- myapp/resources/views/pessoas/create.blade.php
+++ myapp/resources/views/pessoas/create.blade.php
@@ -1,59 +1,57 @@
 @extends('base')
 @section('content')
 
-    <div class="mx-auto" style="width: 1350px;">
-
-            <h5>
-                Lista de usuários cadastrados:
-            </h5>
-            <a class="btn btn-primary" href="{{ route('pessoas.index') }}"> <i class="fa-solid fa-rectangle-list"></i> Listar</a>
-
-        <br>
-        <br>
-
-        <h1>
-            Formulário de cadastro de pessoas
-        </h1>
-        <br>
-
-        @if (!empty(session('success')))
-            {{ session('success') }}
-        @endif
-
-        @if ($errors->all())
-            @foreach ($errors->all() as $error)
-                <div class="alert alert-danger">
-                    {{ $error }}
-                </div>
-            @endforeach
-        @endif
-
-        <form method="POST" action="{{ route('pessoas.store') }}">
-            @csrf
-            <div class="form-group">
-                <label for="name">Nome: </label>
-                <input type="text" class="form-control" name="nome" required />
-            </div>
-            <div class="form-group">
-                <label for="sobrenome">Sobrenome: </label>
-                <input type="text" class="form-control" name="sobrenome" required />
-            </div>
-            <div class="form-group">
-                <label for="endereco">Endereço: </label>
-                <input type="text" class="form-control" name="endereco" required />
-            </div>
-            <div class="form-group">
-                <label for="telefone">Telefone: </label>
-                <input type="text" class="form-control" name="telefone" required />
-            </div>
-            <div class="form-group">
-                <label for="cpf">CPF: </label>
-                <input type="text" class="form-control" name="cpf" required />
-            </div>
+    <div class="mx-auto row">
+        <div class="col-12">
+            <h1 class="text-center">
+                Formulário de cadastro de pessoas
+            </h1>
             <br>
-            <button class="btn btn-primary" type="submit">Enviar</button>
-        </form>
-        <br>
+            <a class="btn btn-primary" href="{{ route('pessoas.index') }}"> <i class="fa-solid fa-rectangle-list"></i> Listar
+                usuários</a>
+            <br>
+            <br>
+            <br>
+
+            @if (!empty(session('success')))
+                {{ session('success') }}
+            @endif
+
+            @if ($errors->all())
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger">
+                        {{ $error }}
+                    </div>
+                @endforeach
+            @endif
+
+            <form method="POST" action="{{ route('pessoas.store') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Nome: </label>
+                    <input type="text" class="form-control" name="nome" required />
+                </div>
+                <div class="form-group">
+                    <label for="sobrenome">Sobrenome: </label>
+                    <input type="text" class="form-control" name="sobrenome" required />
+                </div>
+                <div class="form-group">
+                    <label for="endereco">Endereço: </label>
+                    <input type="text" class="form-control" name="endereco" required />
+                </div>
+                <div class="form-group">
+                    <label for="telefone">Telefone: </label>
+                    <input type="text" class="form-control" name="telefone" required />
+                </div>
+                <div class="form-group">
+                    <label for="cpf">CPF: </label>
+                    <input type="text" class="form-control" name="cpf" required />
+                </div>
+                <br>
+                <button class="btn btn-primary" type="submit">Enviar</button>
+            </form>
+            <br>
+        </div>
     </div>
 
 
--- myapp/resources/views/pessoas/index.blade.php
+++ myapp/resources/views/pessoas/index.blade.php
@@ -1,40 +1,42 @@
 @extends('base')
 @section('content')
-
-    <div class="mx-auto" style="width: 1330px;">
-        <h5>
-            Cadastro de usuários:
-        </h5>
-        <a class="btn btn-success" href="{{ route('pessoas.create') }}"> <i class="fa-solid fa-plus"></i> Cadastrar</a>
-        <br>
-        <br>
-        <table class="table table-striped">
-            <h1>
+    <div class="mx-auto row">
+        <div class="col-12">
+            <h1 class="text-center">
                 Lista de usuários cadastrados
             </h1>
-            <tr>
-                <th>Nome</th>
-                <th>Sobrenome</th>
-                <th>Endereco</th>
-                <th>Telefone</th>
-                <th>Cpf</th>
-                <th>Ações</th>
-            </tr>
-            @foreach ($Pessoas as $Pessoa)
-                {{-- <a href="{{ route('pessoas.show', $Pessoa) }}"> --}}
-                <tr>
-                    <td>{{ $Pessoa->nome }}</td>
-                    <td>{{ $Pessoa->sobrenome }}</td>
-                    <td>{{ $Pessoa->endereco }}</td>
-                    <td>{{ $Pessoa->telefone }}</td>
-                    <td>{{ $Pessoa->cpf }}</td>
-                    <td><button class="btn btn-primary"> <i class="fa-solid fa-pen-ruler"></i> Editar</button>
-                        <button class="btn btn-danger"> <i class="fa-solid fa-trash-can"></i> Excluir</button>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
+            <br>
+            <a class="btn btn-success" href="{{ route('pessoas.create') }}"> <i class="fa-solid fa-plus"></i> Cadastrar
+                usuário</a>
+            <br>
+            <br>
+            <br>
+            <table class="table table-striped">
 
-        {{ $Pessoas->links() }}
+                <tr>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>Endereco</th>
+                    <th>Telefone</th>
+                    <th>Cpf</th>
+                    <th>Ações</th>
+                </tr>
+                @foreach ($Pessoas as $Pessoa)
+                    {{-- <a href="{{ route('pessoas.show', $Pessoa) }}"> --}}
+                    <tr>
+                        <td>{{ $Pessoa->nome }}</td>
+                        <td>{{ $Pessoa->sobrenome }}</td>
+                        <td>{{ $Pessoa->endereco }}</td>
+                        <td>{{ $Pessoa->telefone }}</td>
+                        <td>{{ $Pessoa->cpf }}</td>
+                        <td><button class="btn btn-primary"> <i class="fa-solid fa-pen-ruler"></i> Editar</button>
+                            <button class="btn btn-danger"> <i class="fa-solid fa-trash-can"></i> Excluir</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+
+            {{ $Pessoas->links() }}
+        </div>
     </div>
 @endsection
